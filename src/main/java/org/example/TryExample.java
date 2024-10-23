612928bf72eab7e2279d8ac1e514a33d0de3ffb5
G00403364@atu.ie <G00403364@atu.ie>
2024-10-23 16:50:19 +0100

    Try and Catch

---
+++ src/main/java/org/example/TryExample.java
@@ -0,0 +1,23 @@
+package org.example;
+import java.util.Scanner;
+
+public class TryExample {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter a number between 0 - 9 :");
+        Scanner myScan = new Scanner(System.in);
+
+        String input = myScan.nextLine();
+
+        int myNum = Integer.parseInt(input);
+
+        if (myNum >= 0 && myNum <= 9) {
+            System.out.println("You entered " + myNum);
+        }
+        else {
+            System.out.println("Not a valid number");
+        }
+    }
+
+}
+
